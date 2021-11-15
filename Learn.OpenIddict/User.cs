261f1ba4bcd5f0ecba63d38f64cbb3c58ee5213e
Oluwasegun Akinpelu <82780450+oluwasege@users.noreply.github.com>
2021-11-15 13:25:00 +0100

    Add project files.

---
+++ Learn.OpenIddict/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Learn.OpenIddict
+{
+    public class User: IdentityUser<long>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
